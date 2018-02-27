35e9ea9c46bd9ffb8401ae781311090efd60f675
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-02-27 14:45:00 -0800

    groceery

--- main.cpp
+++ main.cpp
@@ -1,53 +1,49 @@
-// Author: Sultan Alshamsi
+//Author: Sultan Alshamsi
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-string list[5]; //array of 5 strings
-int numItems = 0;
-char input;
-string input2;
-do
- {
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string input2;
 
- cout<<"==GROCERY LIST MANAGER=="<<endl;
- cout<<"Enter your choice: "<<endl;
- cout<<" (A)dd an item"<<endl;
- cout<<" (Q)uit"<<endl;
- cout<<"Your choice (A/Q): "<<endl;
- cin>>input;
- if(input == 'A' || input == 'a')
-  {
- cout<<"What is the item?\n";
- cin>>input2;
- if(numItems<5)
- {
- list[numItems]=input2;
- numItems++;
- }
- else
- {
- cout<<"You'll need a bigger list!\n";
- }
- }
-}  while(input!='q' && input!='Q');
+        do
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): "<<std::flush;
+                cin>>input;
+                if(input == 'A' || input == 'a')
+                {
+                        cout<<"What is the item?\n";
+                        cin>>input2;
 
-    cout<<"==ITEMS TO BUY==";
+                        if(numItems<5)
+                        {
+                                list[numItems]=input2;
+                                numItems++;
+                        }else
+                        {
+                                cout<<"You'll need a bigger list!\n";
+                        }
+                }
+        }while(input!='q' && input!='Q');
 
-   for(int numItems=0; numItems<5; numItems++)
-{
+        cout<<"==ITEMS TO BUY==";
+        for(int numItems=0; numItems<5; numItems++)
+        {
+                cout<<endl<<numItems+1 << " ";
 
-    cout<<endl<<numItems+1 << " ";
+                if(list[numItems]!="")
+                {
+                        cout<<list[numItems];
+                }
+        }cout<<endl;
 
-    if(list[numItems]!="")
-{
-
-cout<<list[numItems];
+        return 0;
 }
-}cout<<endl;
-
-
-return 0;
- }
